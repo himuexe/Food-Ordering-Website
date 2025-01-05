ac9141b54bb467daa4fbd178c6bfef963990a17e
Himanshu Sharma <sharmaslov@gmail.com>
2025-01-05 16:37:20 +0530

    Landing Page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,13 @@ import express from  "express"
 import cors from "cors"
 import "dotenv/config"
 import {Request,Response} from "express"
+import mongoose from "mongoose"
 
 
 const app = express();
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
+    console.log("connected to database")
+});
 
 app.use(express.json());
 app.use(cors());
